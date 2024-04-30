09baa2fe516dac5c437fdbe6a2e9d8fae9ab37e8
kirti <manalipatil18@gmail.com>
2024-04-30 13:12:18 -0500

    Commit all

---
+++ src/test/java/utilities/Helper.java
@@ -0,0 +1,37 @@
+package utilities;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+
+public class Helper {
+ 
+	private static WebDriver driver;
+	
+		public void navigateBack() {
+			
+		driver.navigate().back();
+		}
+
+		public void navigateForward() {
+
+		driver.navigate().forward();
+
+		}
+		public String getTitleOfThePage() 
+		{
+		return driver.getTitle();
+		}
+		
+		public static boolean validateErrorMsg(String errorMsg, String expectedErrorMsg) {
+			boolean flag = false;
+			try {
+				// assertEquals(true, errorMsg.contains(expectedErrorMsg));
+				assertEquals(errorMsg, expectedErrorMsg);
+				flag = true;
+			} catch (Exception e) {
+				flag = false;
+			}
+			return flag;
+		}
+}
