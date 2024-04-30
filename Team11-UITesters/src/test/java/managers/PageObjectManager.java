bd81e1873616f4510815cce846cd24c5cc45aca1
VD136 <150267565+VD136@users.noreply.github.com>
2024-04-30 16:32:23 -0400

    Update PageObjectManager.java

--- Team11-UITesters/src/test/java/managers/PageObjectManager.java
+++ Team11-UITesters/src/test/java/managers/PageObjectManager.java
@@ -4,19 +4,20 @@ import org.openqa.selenium.WebDriver;
 
 import pages.*;
 
-public class PageObjectManager {
+public class PageObjectManager<ProgramDelPage> {
 	
+	private static final String ProgramdelPage = null;
 	private WebDriver driver;
-	
-	private HomePage homePage;
-	
+    private HomePage homePage;
 	private Userpagevalidation userpagevalidation;
-	
 	private AddNewUser adduser;
 
-	
-	
-	
+	private LoginPage loginPage;
+	private ProgramdelPage programdelpage;
+	private ProgramMdelPage programMdelPage;
+	private ProgramNavPage programnavPage;
+	private ProgramsortPage programsortpage;
+
 	public PageObjectManager(WebDriver driver) 
 	{
 		this.driver = driver;
@@ -27,15 +28,37 @@ public class PageObjectManager {
 	{
 		return (homePage == null) ? homePage = new HomePage(driver) : homePage;
 	}
-	
-	public Userpagevalidation getUserpagevalidation()
-	{
+
+	public Userpagevalidation getUserpagevalidation() {
+
 		return(userpagevalidation == null) ? userpagevalidation = new Userpagevalidation(driver) : userpagevalidation;
 	}
-	
+    public ProgramdelPage getprogramdelPage() {
+		return (programdelpage == null) ? programdelpage = new ProgramdelPage(driver) : programdelpage;
+}
+
 	public AddNewUser getAddUser()
 	{
 		return(adduser == null) ? adduser = new AddNewUser(driver) : adduser;
 	}
+	public ProgramMdelPage getProgramMDelPage() {
+		return (programMdelPage == null) ? programMdelPage = new ProgramMdelPage(driver) : programMdelPage;
+	}
+
+	public LoginPage getLoginPage()
+	{
+		return (loginPage == null) ? loginPage = new LoginPage(driver) : loginPage;
+	}
 	
+	public ProgramNavPage getProgramNavPage()
+	{
+		return (programnavPage == null) ? programnavPage = new ProgramNavPage(driver) : programnavPage;
+	}
+	public ProgramsortPage getProgramsortPage()
+	{
+		return (programsortpage == null) ? programsortpage = new ProgramsortPage(driver) : programsortpage;
+	}
+
 }
+	
+	
