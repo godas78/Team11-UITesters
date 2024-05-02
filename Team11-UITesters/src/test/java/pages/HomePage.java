f9743df4f534988555ed5bb11258a700aee737c8
Sravanthib200 <sravanthib200@gmail.com>
2024-05-01 22:57:36 -0400

    homepage changes

--- Team11-UITesters/src/test/java/pages/HomePage.java
+++ Team11-UITesters/src/test/java/pages/HomePage.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ExecutionException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -25,7 +26,6 @@ import managers.FileReaderManager;
 import readers.ConfigFileReader;
 import readers.ExcelReader;
 import utilities.Log;
-
 import utilities.Screenshots;
 
 
@@ -46,11 +46,14 @@ public class HomePage {
     @FindBy(xpath="//*[@id='mat-form-field-label-1']/span[1]") WebElement usertext;
     @FindBy(xpath="//*[@id='mat-form-field-label-3']/span[1]") WebElement passwordtext;
     @FindBy(id="password") WebElement password;
+//    @FindBy (xpath  = "//a[text()='Customers']") WebElement learningtext ;	
+//	 @FindBy (xpath  = "//a[text()='Management']") WebElement managementtext ;
+//	 @FindBy (xpath  = "//a[text()='Systems']") WebElement systemtext;
 	@FindBy (xpath  = "//button[@id='login']") WebElement loginBtn;
 	 @FindBy (xpath  = "//[@id='login']/span[1]") WebElement logintext ;
 	 @FindBy (xpath  = "//button[@id='login']") WebElement loginpageloginbtn;
-	//img[@src='assets/img/LMS-logo.jpg']--logo
-	 @FindBy (xpath  = "//icon") WebElement logoimage ;
+	 @FindBy (xpath  ="//img[@src='assets/img/LMS-logo.jpg']") WebElement logoimage;
+	// @FindBy (xpath  = "//icon") WebElement logoimage ;
 	 @FindBy (xpath  = "//div/mat-card/mat-card-content/form/p") WebElement loginpageheader;
 	 @FindBy (xpath  = "//*[@id='mat-form-field-label-1']/span[2]") WebElement usernameasterick;
 	 @FindBy (xpath  = "//*[@id='mat-form-field-label-3']/span[2]") WebElement passwordasterick;	
@@ -86,52 +89,9 @@ public class HomePage {
 	public void invalidURLstatus() {
 		assertTrue(driver.getTitle().contains("404"));
 		
-//		String title = driver.getTitle();
-//        if (title.contains("404")) {
-//            System.out.println("404 Page not found error received for invalid URL");
-//        } else {
-//            System.out.println("Invalid URL did not return a 404 error");
-//        }
 	}
 	public void broken_links()  {
-//List<WebElement> links = driver.findElements(By.tagName("a"));
-//        
-//        Iterator<WebElement> it = links.iterator();
-//        
-//        while(it.hasNext()){
-//            
-//        	LinkURL = it.next().getAttribute("href");
-//            
-//            System.out.println(LinkURL);
-//		
-//		if (LinkURL==null || LinkURL.isEmpty()) {
-//			Log.info("URL is either not configurerd for anchor tag or it is empty");
-//		}
-//		
-//		try {
-//            huc = (HttpURLConnection)(new URL(LinkURL).openConnection());
-//            
-//            huc.setRequestMethod("HEAD");
-//            
-//            huc.connect(); 
-//            
-//            respCode = huc.getResponseCode();
-//            
-//            if(respCode >= 400){
-//                System.out.println(LinkURL+" is a broken link");
-//            }
-//            else{
-//                System.out.println(LinkURL+" is a valid link");
-//            }
-//		} catch (MalformedURLException e) {
-//          
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            
-//            e.printStackTrace();
-//        }
-//	}
-//	}
+
 		String url= driver.getCurrentUrl();
 		
 		try {		
@@ -151,25 +111,42 @@ public class HomePage {
 		}	
 	}
 	
- 
-
-		
-	
-
 	public void spellchecker() {
-		// TODO Auto-generated method stub
-		
+//		String textLearning = learningtext.getText();
+//		 Assert.assertEquals("LEARNING",textLearning);
+//			System.out.println(textLearning);
+//			
+//			String textmanagementtext = managementtext.getText();
+//			 Assert.assertEquals("MANAGEMENT",textmanagementtext);
+//			 System.out.println(textmanagementtext);
+//			
+//			 String textsystemtext = systemtext.getText();
+//			 Assert.assertEquals("SYSTEM",textsystemtext);
+//			 System.out.println(textsystemtext);
+//			 
+//			 String textlogintext = logintext.getText();
+//			 Assert.assertEquals("LOGIN",textlogintext);
+//			 System.out.println(textlogintext);
+			
 	}
 
-	public void correctlogo(Object object) {
-		// TODO Auto-generated method stub
+	public void correctlogo(WebElement img) {
+		img=logoimage;
 		
 	}
 	
 
 	public void logoaligned() {
-		// TODO Auto-generated method stub
-		
+Boolean ImagePresent = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", logoimage);
+	    
+		if (!ImagePresent)
+        {
+        System.out.println("Image not displayed.");
+        }
+        else
+        {
+        System.out.println("Image is displayed.");
+        }
 	}
 
 	public void loginbtncheck() {
@@ -193,30 +170,24 @@ public class HomePage {
 	}
 
 	public void textcheck(int i) {
-		// TODO Auto-generated method stub
+		List<WebElement> textfields = driver.findElements(By.xpath("//input[@id='username' or @id='password']"));
+		int size = textfields.size();
+		if(size==i) {
+			Log.info("2 text feilds are present");
+		}
+		else {
+			Log.info("text feilds are not 2");
+		}
 		
 	}
 
 	public String usertext() {
 		return usertext.getText();
 	}
-//		String expectedmsg = string;
-//		String actualmsg = username.getText();
-//		Assert.assertEquals(expectedmsg,actualmsg);
-//		 System.out.println(actualmsg);
-//		
-//	}
-//	public void usertext() {
-//		username.getText();
-//	}
 
 	public String passwordtext() {
 		return passwordtext.getText();
-//		String expectedmsg = string;
-//		String actualmsg = password.getText();
-//		Assert.assertEquals(expectedmsg,actualmsg);
-//		 System.out.println(actualmsg);
-		
+	
 	}
 	public void astrikuser(String string) {
 		String expectedmsg = string;
